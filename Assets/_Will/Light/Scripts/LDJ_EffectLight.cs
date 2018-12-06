3b239c2991a4bb7aef6a5842c5157d5816417456
Will <william.comminges@gmail.com>
2018-12-06 14:06:09 +0100

    yo le rap

---
+++ Assets/_Will/Light/Scripts/LDJ_EffectLight.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class LDJ_EffectLight : MonoBehaviour
+{
+    #region F/P
+    Light thisLight;
+    float lightIntensity;
+    //[SerializeField]
+    //float minIntensity = 2f;
+    [SerializeField]
+    float maxIntensity = 5f;
+    [SerializeField]
+    Color lightColor;
+    [SerializeField]
+    float ChangeIntensitySpeed = 1f;
+
+    #endregion
+
+    #region Meths
+    void changeLightSettings()
+    {
+        lightIntensity = Mathf.PingPong(Time.time * ChangeIntensitySpeed,maxIntensity);
+        thisLight.intensity = lightIntensity;
+        thisLight.color = lightColor;
+    }
+    void GetLight()
+    {
+        thisLight = GetComponent<Light>();
+    }
+    #endregion
+
+    #region UniMeths
+    private void Start()
+    {
+        GetLight();
+    }
+    private void Update()
+    {
+        changeLightSettings();
+    }
+    #endregion
+}
