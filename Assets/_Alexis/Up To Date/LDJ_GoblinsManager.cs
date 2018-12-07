ead1e9f15ffbec7402cc8c1e4e07fd506f699a6e
Alexis Thiébaut <thiebautalexis01@gmail.com>
2018-12-07 16:26:32 +0100

    Change Horde

--- Assets/_Alexis/Up To Date/LDJ_GoblinsManager.cs
+++ Assets/_Alexis/Up To Date/LDJ_GoblinsManager.cs
@@ -10,7 +10,7 @@ public class LDJ_GoblinsManager : MonoBehaviour
     #region Events
     public event Action OnHordeActivated;
     public event Action OnHordeHit;
-    public event Action OnPlayerHit; 
+    public event Action<float> OnPlayerHit; 
     public Action<float> OnSpeedModified; 
     #endregion
 
@@ -22,18 +22,24 @@ public class LDJ_GoblinsManager : MonoBehaviour
     [Header("Horde Informations")]
     [SerializeField] Transform hordeParent; 
     [SerializeField] Transform hordeTarget;
+    [Header("Speed")]
     [SerializeField, Range(1, 50)] float minSpeed = 1;
     [SerializeField, Range(1, 50)] float maxSpeed = 50;
     [SerializeField, Range(1, 50)] float currentSpeed = 1;
-    [SerializeField, Range(1, 100)] int loosingSpeedPercentage = 10; 
+    [SerializeField, Range(1, 10)] int decreasingSpeedValue = 10;
+    [SerializeField, Range(1, 10)] int increasingSpeedValue = 5;
+    [SerializeField, Range(1, 300)] int increasingSpeedRange = 25; 
 
     private LDJ_GoblinAgent leader;
 
-    [Header("Invincibility Settings")]
-    private bool canMove = true;
     #endregion
 
     #region UnityMethods
+    private void Start()
+    {
+        InvokeRepeating("IncreaseSpeed", increasingSpeedRange, increasingSpeedRange);     
+    }
+
     private void Awake()
     {
         OnHordeActivated += InitHorde;
@@ -54,6 +60,12 @@ public class LDJ_GoblinsManager : MonoBehaviour
     #endregion
 
     #region Methods
+    /// <summary>
+    /// Initialize the horde
+    /// Instantiate every agent
+    /// Set them a speed and a target
+    /// Add Callback on events
+    /// </summary>
     private void InitHorde()
     {
         if(instanciedAgent)
@@ -63,7 +75,9 @@ public class LDJ_GoblinsManager : MonoBehaviour
             {
                 _goblin = Instantiate(instanciedAgent, transform.position + new Vector3(Random.Range(0, spawningRange), 0, Random.Range(0, spawningRange)), Quaternion.identity, hordeParent ? hordeParent : null);
                 OnSpeedModified += _goblin.SetSpeed;
-                _goblin.OnAgentHit += HitHorde; 
+                _goblin.OnAgentHit += HitHorde;
+                _goblin.OnHitPlayer += HitPlayer; 
+                OnPlayerHit += _goblin.StopMovement; 
                 if (i == 0)
                 {
                     leader = _goblin;
@@ -78,35 +92,48 @@ public class LDJ_GoblinsManager : MonoBehaviour
     }
 
     /// <summary>
+    /// Call the method Change Speed with percentage with the increasingSpeedPercentages
+    /// </summary>
+    private void IncreaseSpeed()
+    {
+        ApplyNewSpeed(increasingSpeedValue); 
+    }
+
+    /// <summary>
     /// Change speed value using a percentage
     /// The value is calculated with a percentage based on the total speed range
     /// </summary>
     /// <param name="_percentage"></param>
-    public void ChangeSpeedWithPercentage(float _percentage)
+    public void ApplyNewSpeed(float _value)
     {
-        _percentage = Mathf.Clamp(_percentage, -100, 100);
-        bool _increase = _percentage > 0;
-        float _value = ((maxSpeed - minSpeed) * Mathf.Abs(_percentage)) / 100;
-        if (_increase)
-            currentSpeed += _value;
-        else
-            currentSpeed -= _value;
+        //_percentage = Mathf.Clamp(_percentage, -100, 100);
+        //bool _increase = _percentage > 0;
+        //float _value = ((maxSpeed - minSpeed) * Mathf.Abs(_percentage)) / 100;
+        //if (_increase)
+        //    currentSpeed += _value;
+        //else
+        currentSpeed += _value;
         currentSpeed = Mathf.Clamp(currentSpeed, minSpeed, maxSpeed);
         OnSpeedModified?.Invoke(currentSpeed);
     }
 
     /// <summary>
     /// CALLED WHEN THE HORDE IS HIT 
+    /// slow down the horde and every agent
     /// </summary>
     public void HitHorde()
     {
         OnHordeHit?.Invoke();
-        ChangeSpeedWithPercentage(-loosingSpeedPercentage);
+        ApplyNewSpeed(-decreasingSpeedValue);
     }
 
+    /// <summary>
+    /// Called when the player is hit
+    /// Apply stop on each agent
+    /// </summary>
     public void HitPlayer()
     {
-        OnPlayerHit?.Invoke(); 
+        OnPlayerHit?.Invoke(minSpeed); 
     }
     #endregion 
 }
